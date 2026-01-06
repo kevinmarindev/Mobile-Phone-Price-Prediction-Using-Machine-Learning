9c5805f0ecf79cee6d6c7f690531d3325bc50eb0
kevinmarindev <kevin.marin7@hotmail.com>
2026-01-06 16:52:41 -0600

    cleaning and preparing data for model

---
+++ model_price_model.py
@@ -0,0 +1,33 @@
+# B1  Data preprocessing
+#####################################################
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+
+
+
+
+# 1. Load the dataset
+df = pd.read_csv("mobile_price_prediction_with_names.csv")
+
+# 2. Check for missing values
+print("Missing values:")
+print(df.isnull().sum())
+
+# 3. Convert  column (supports_5G) to numeric values (1 for True, 0 for False)
+df["supports_5G"] = df["supports_5G"].astype(int)
+
+# 4. Drop mobile_name because it is not useful for prediction
+df = df.drop(columns=["mobile_name"])
+
+# 5. Separate features and target variable
+X = df.drop("price_USD", axis=1)
+y = df["price_USD"]
+
+# 6. Scale the feature values
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
